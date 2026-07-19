d8376f0fecccf28aaa292632d2a406c4e3005b07
Res0x <toha3.reshetilo@gmail.com>
2026-07-19 23:10:44 +0300

    added game_details page

--- games/urls.py
+++ games/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('games/', views.game_list, name='game_list'),
     path('ratings/', views.ratings, name='ratings'),
     path('games/add/', views.game_create, name='game_create'),
+    path(
+        'games/<slug:game_slug>/',
+        views.game_detail,
+        name='game_detail',
+    ),
 ]
--- games/views.py
+++ games/views.py
@@ -1,5 +1,92 @@
 from django.shortcuts import render
+from django.http import Http404
 
+GAME_DETAILS = {
+    'witcher-3': {
+        'slug': 'witcher-3',
+        'title': 'The Witcher 3: Wild Hunt',
+        'subtitle': 'Большое сюжетное приключение в мрачном фэнтезийном мире',
+        'genre': 'RPG',
+        'platform': 'PC',
+        'status': 'Пройдено',
+        'rating': 10,
+        'developer': 'CD Projekt Red',
+        'release_year': 2015,
+        'playtime': '120 часов',
+        'cover': 'games/images/witcher-3.svg',
+        'cover_caption': 'Обложка The Witcher 3: Wild Hunt',
+        'description': (
+            'История охотника на чудовищ, который путешествует по открытому '
+            'миру, выполняет заказы и принимает решения, влияющие на сюжет.'
+        ),
+        'highlights': [
+            'Большой открытый мир с самостоятельными историями.',
+            'Развитая система заданий и диалогов.',
+            'Решения игрока влияют на развитие событий.',
+            'Большое количество дополнительного контента.',
+        ],
+        'recommendation_slugs': [
+            'cyberpunk-2077',
+            'hollow-knight',
+        ],
+    },
+    'cyberpunk-2077': {
+        'slug': 'cyberpunk-2077',
+        'title': 'Cyberpunk 2077',
+        'subtitle': 'Футуристическая RPG о наемнике из Найт-Сити',
+        'genre': 'Action RPG',
+        'platform': 'PC',
+        'status': 'Прохожу',
+        'rating': 9,
+        'developer': 'CD Projekt Red',
+        'release_year': 2020,
+        'playtime': '85 часов',
+        'cover': 'games/images/cyberpunk-2077.svg',
+        'cover_caption': 'Обложка Cyberpunk 2077',
+        'description': (
+            'Приключение в технологичном мегаполисе, где игрок развивает '
+            'персонажа, выполняет задания и выбирает собственный стиль игры.'
+        ),
+        'highlights': [
+            'Атмосферный город с большим количеством районов.',
+            'Разные способы выполнения заданий.',
+            'Гибкая система развития персонажа.',
+            'Сюжетные и дополнительные цепочки заданий.',
+        ],
+        'recommendation_slugs': [
+            'witcher-3',
+            'hollow-knight',
+        ],
+    },
+    'hollow-knight': {
+        'slug': 'hollow-knight',
+        'title': 'Hollow Knight',
+        'subtitle': 'Исследование таинственного подземного королевства',
+        'genre': 'Metroidvania',
+        'platform': 'PC',
+        'status': 'Хочу пройти',
+        'rating': None,
+        'developer': 'Team Cherry',
+        'release_year': 2017,
+        'playtime': 'Не указано',
+        'cover': 'games/images/hollow-knight.svg',
+        'cover_caption': 'Обложка Hollow Knight',
+        'description': (
+            'Атмосферное приключение с исследованием связанных локаций, '
+            'сражениями, поиском секретов и постепенным открытием новых путей.'
+        ),
+        'highlights': [
+            'Связанный мир с большим количеством секретов.',
+            'Выразительный визуальный стиль.',
+            'Разнообразные противники и боссы.',
+            'Постепенное открытие новых способностей.',
+        ],
+        'recommendation_slugs': [
+            'witcher-3',
+            'cyberpunk-2077',
+        ],
+    },
+}
 
 def home(request):
     context = {
@@ -40,6 +127,7 @@ def game_list(request):
             'rating': 10,
             'cover': 'games/images/witcher-3.svg',
             'cover_caption': 'Фэнтезийное приключение в открытом мире',
+            'slug': 'witcher-3',
         },
         {
             'id': 2,
@@ -50,6 +138,7 @@ def game_list(request):
             'rating': 9,
             'cover': 'games/images/cyberpunk-2077.svg',
             'cover_caption': 'История в неоновом мегаполисе будущего',
+            'slug': 'cyberpunk-2077',
         },
         {
             'id': 3,
@@ -60,6 +149,7 @@ def game_list(request):
             'rating': None,
             'cover': 'games/images/hollow-knight.svg',
             'cover_caption': 'Путешествие по мрачному подземному королевству',
+            'slug': 'hollow-knight',
         },
     ]
 
@@ -172,3 +262,23 @@ def game_create(request):
     }
 
     return render(request, 'games/game_create.html', context)
+
+def game_detail(request, game_slug):
+    game = GAME_DETAILS.get(game_slug)
+
+    if game is None:
+        raise Http404('Игра не найдена')
+
+    recommendations = [
+        GAME_DETAILS[slug]
+        for slug in game['recommendation_slugs']
+        if slug in GAME_DETAILS
+    ]
+
+    context = {
+        'page_title': game['title'],
+        'game': game,
+        'recommendations': recommendations,
+    }
+
+    return render(request, 'games/game_detail.html', context)
